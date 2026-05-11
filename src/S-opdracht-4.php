a55b9014d06351800d2717d68c20271bb2ec7e6f
kim <kf.dejaeger@gmail.com>
2026-05-11 21:00:01 +0200

    update S-opdracht 4

---
+++ src/S-opdracht-4.php
@@ -0,0 +1,8 @@
+ <!-- Opdracht 4:
+    Verander de string van opdracht 1 naar uppercase,
+    verander de string van opdracht 1 naar lowercase
+ -->
+<?php
+echo strtoupper("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, help when an unknown printer took a galley<br>");
+echo strtolower("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, help when an unknown printer took a galley");
+?>
