8def9e101803fb7d083b4449c6f71eee26bfe579
kim <kf.dejaeger@gmail.com>
2026-06-19 16:09:15 +0200

    register formulier aangepast

--- src/register.php
+++ src/register.php
@@ -1,13 +1,17 @@
 <?php
 $conn = require_once "partials/dbconnection-kim.php";
 
-$formUsername = $_POST['name'] ?? '';
+$formUsername = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
-
+$voornaam = $_POST['Voornaam'] ?? '';
+$tussenvoegsel = $_POST['tussenvoegsel'] ?? '';
+$achternaam = $_POST['achternaam'] ?? '';
+$geboortedatum = $_POST['geboortedatum'] ?? '';
+$email = $_POST['email'] ?? '';
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-$stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-$stmt->bind_param("ss", $formUsername, $hashedPassword);
+$stmt = $conn->prepare("INSERT INTO users (username, password, voornaam, tussenvoegsel, achternaam, geboortedatum, email) VALUES (?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssssss", $formUsername, $hashedPassword, $voornaam, $tussenvoegsel, $achternaam, $geboortedatum, $email);
 $stmt->execute();
 
 if ($stmt->affected_rows === 1) {
